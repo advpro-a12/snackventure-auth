45b2cf0bfd312675dd815057446a617b1009e013
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-23 19:16:39 +0700

    feat: add userId and roles claims to JWT token

--- src/main/java/id/ac/ui/cs/advprog/snackventure/auth/security/SecurityConstants.java
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/auth/security/SecurityConstants.java
@@ -1,5 +1,5 @@
 package id.ac.ui.cs.advprog.snackventure.auth.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 1000 * 60 * 60 * 24;
 }
